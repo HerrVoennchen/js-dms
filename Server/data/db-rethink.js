b2033a0dd3ed35a3e4730b13d547eabfc43c7a92
Sebastian <sebastian.kruemling@gmail.com>
2015-05-28 21:55:14 +0200

    update packages und rethinkdbdash

--- Server/data/db-rethink.js
+++ Server/data/db-rethink.js
@@ -1,82 +1,44 @@
-var r = require('rethinkdb');
 var config = require('../config/config.json');
-
+var r = require('rethinkdbdash')({ servers: config.database.hosts });
 
 exports.listAllUser = function(callback) {
-    r.connect({ host: config.database.server, port: config.database.port }, function(err, conn) {
-        if(err) throw err;
-
-        r.db(config.database.dbname).table('users').run(conn, function(err, res) {
-
-            res.toArray(function(err, docs) {
-                callback(err, docs);
-                conn.close(function(err) { if (err) throw err; });
-            });
-        });
+    r.db(config.database.dbname).table('users').run(function(err, res) {
+        callback(err, res);
     });
 };
 
 
 exports.addUser = function(userObject, callback) {
-    r.connect({ host: config.database.server, port: config.database.port }, function(err, conn) {
-        if(err) throw err;
-
-        r.db(config.database.dbname).table('users').insert(userObject).run(conn, function(err, res) {
-            callback(err);
-            conn.close(function(err) { if (err) throw err; });
-        });
+    r.db(config.database.dbname).table('users').insert(userObject).run(function(err, res) {
+        callback(err);
     });
 }
 ;
 
 exports.updateUser = function(userObject, callback) {
-    r.connect({ host: config.database.server, port: config.database.port }, function(err, conn) {
-        if(err) throw err;
-
-        r.db(config.database.dbname).table('users').update(userObject).run(conn, function(err, res) {
-            callback(err);
-            conn.close(function(err) { if (err) throw err; });
-        });
+    r.db(config.database.dbname).table('users').update(userObject).run(function(err, res) {
+        callback(err);
     });
 };
 
 exports.getUser = function (id, callback) {
-    r.connect({ host: config.database.server, port: config.database.port }, function(err, conn) {
-        if (err) throw err;
-
-        r.db(config.database.dbname).table('users').get(id).run(conn, function(err, res) {
-            callback(err, res);
-            conn.close(function(err) { if (err) throw err; });
-        });
+    r.db(config.database.dbname).table('users').get(id).run(function(err, res) {
+        callback(err, res);
     });
 }
 
 exports.deleteUser = function(id, callback) {
-    r.connect({ host: config.database.server, port: config.database.port }, function(err, conn) {
-        if (err) throw err;
-
-        r.db(config.database.dbname).table('users').get(id).delete().run(conn, function(err, res) {
-            callback(err);
-            conn.close(function(err) { if (err) throw err; });
-        });
+    r.db(config.database.dbname).table('users').get(id).delete().run(function(err, res) {
+        callback(err);
     });
 };
 
 exports.searchDocs = function (docObj, callback) {
-    r.connect({ host: config.database.server, port: config.database.port }, function(err, conn) {
-        if(err) throw err;
 
-
-    });
 };
 
 exports.addDocument = function (document, callback) {
-    r.connect({ host: config.database.server, port: config.database.port }, function(err, conn) {
-        if(err) throw err;
-
-        r.db(config.database.dbname).table('documents').insert(document).run(conn, function(err, res) {
-            callback(err);
-            conn.close(function(err) { if (err) throw err; });
-        });
+    r.db(config.database.dbname).table('documents').insert(document).run(function(err, res) {
+        callback(err);
     });
 };
